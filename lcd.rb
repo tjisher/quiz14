8023b61069bc8718507af5d689b7378f8d2c0df8
tjisher <tjisher@gmail.com>
2016-02-19 19:15:18 +0000

    Converted main function to static

--- lcd.rb
+++ lcd.rb
@@ -1,28 +1,28 @@
 ##ToC
+# LCDStatic Module
 # LCD Class
 # Console Runner
 
-## LCD Class
+## LCDStatic Module
+# static and support for LCD Class
+# provides no exception protection, conversion helpers or storage
 # converts characters to simplified 5 layer grid, scaleable
 #
 # case sensative
-# not implemented characters are ignored, completely invalid entry will return empty string
-# works with any object convertable to string
+# not implemented characters are ignored, completely invalid string convertable /
+# will return empty string
 #
-# default to_s overides:
-# time object specificly ouputs in HH:MM format
+# default method overides:
+# none
 #
 # key methods:
-# display, takes key values are ouputs result to console
-# update_output, actual string generation
+# create_output
 #
 # encoding details:
 # top horizontal, top vertical, mid horizontal, bottom vertical, bottom horizontal
 # none/all, left/right/all, none/all, left/right/all, none/all
 # note: horizontal always has space left and rightmost ie " -- "
-class LCD
-	attr_accessor :scale, :character_encoding, :horizontal_character, :vertical_character
-	attr_reader :values, :output, :logger
+module LCDStatic
 	CHARACTER_ENCODING_DEFAULT = {
 		" " => [0, 0, 0 ,0, 0],
 		"0" => [1, 3, 0 ,3, 1],
@@ -41,6 +41,120 @@ class LCD
 	VERTICAL_CHARACTER_DEFAULT = "|"
 	SPECIAL_CHARACTER = "."
 	SCALE_DEFAULT = 2
+
+	# generate new lcd representation
+	# returns string with new line characters
+	#
+	# parameters:
+	# values, string to be displayed
+	#
+	# optional parameters:
+	# scale, otherwise default 2
+	# logger, otherwise records nothing
+	# character_encoding, otherwise default character set ( \s, :, 0-9)
+	# horizontal_character, otherwise default "-"
+	# vertical_character, otherwise default "|"
+	# special_character, otherwise default "."
+	def create_output(values, scale=SCALE_DEFAULT, logger=nil, 
+			character_encoding=CHARACTER_ENCODING_DEFAULT, 
+			horizontal_character=HORIZONTAL_CHARACTER_DEFAULT,
+			vertical_character=VERTICAL_CHARACTER_DEFAULT,
+			special_character=SPECIAL_CHARACTER)
+		output = ""
+		values = values.to_s
+		return output if values.empty?
+
+		#adapt for scale
+		# key line numbers
+		number_of_lines = (scale * 2)  + 3 # each scale affects 2 verticals, add 3 for horizontals
+		last_line = number_of_lines - 1 #0 based
+		mind_line = scale + 1 #height of one vertical and first horizontal line, 0 based
+
+		# prepare working container
+		display_values = []
+		number_of_lines.times { |line| display_values << "" } 
+
+		# generate the possible elements
+		horizontal_slices = [ " #{' ' * scale} ", # " \s "
+			" #{horizontal_character * scale} "] # ie " - "
+		vertical_slices = [ " #{' ' *  scale} ", # " \s "
+			"#{vertical_character}#{' ' * scale} ", " #{' ' * scale}#{vertical_character}",  #ie  "|\s ", " \s|",
+			"#{vertical_character}#{' ' * scale}#{vertical_character}"] #ie  "|\s|"
+		special_slices = [ " ", "#{special_character}"] #ie "\s", "."
+
+		#actual conversion
+		#character by character, then line by line, appending
+		values.each_char do |char|
+
+			char_on_grid = character_encoding[char]
+			layer = 0 #top horizontal, top vertical, mid horizontal, bottom vertical, bottom horizontal
+
+			number_of_lines.times do |line|
+				display_values[line] += " " unless display_values[line].empty? #add spacing
+
+				#convert the encoding to its segment
+				if char_on_grid == :special
+					#special charaters, dont respect convention, ie ':'
+					if line == (mind_line - 1) || line == (mind_line + 1)
+						#only output on either side of mid line
+						display_values[line] +=  special_slices[1]
+					else
+						display_values[line] +=  special_slices[0]
+					end
+				#normal characters
+				elsif layer.even?
+					#horizontal, strings already scaled
+					display_values[line] += horizontal_slices[ char_on_grid[layer]]
+				else	
+					#vertical, parent loop copes with scaling
+					display_values[line] += vertical_slices[ char_on_grid[layer]]
+				end
+
+				#switch to next layer if needed, ie do not increment if need more vericals
+				if line == 0 ||  line == mind_line
+					#just done a horizontal layer next must be vertical
+					layer += 1
+				elsif line == (last_line - 1) ||line == (mind_line - 1)
+					#next layer is horizontal
+					layer += 1
+				end
+
+			end
+		end
+
+		#convert into single string
+		output = display_values.join( "\n")
+		output += "\n" unless output.empty? #add trailing new line
+
+		logger << { values: values, scale: scale, finished_at: Time.now, 
+			output: output } if logger
+		output
+	end
+
+end
+
+## LCD Class
+# converts characters to simplified 5 layer grid, scaleable
+#
+# case sensative
+# not implemented characters are ignored, completely invalid entry will return empty string
+# works with any object convertable to string
+#
+# default method overides:
+# to_s, time object specificly ouputs in HH:MM format
+#
+# key methods:
+# display, takes key values are ouputs result to console
+# update_output, actual string generation
+#
+# encoding details:
+# top horizontal, top vertical, mid horizontal, bottom vertical, bottom horizontal
+# none/all, left/right/all, none/all, left/right/all, none/all
+# note: horizontal always has space left and rightmost ie " -- "
+class LCD 
+	include LCDStatic
+	attr_accessor :scale, :character_encoding, :horizontal_character, :vertical_character
+	attr_reader :values, :output, :logger
 
 	def initialize
 		#defaults
@@ -104,73 +218,8 @@ class LCD
 	#returns string with new line characters
 	#adds to logger
 	def update_output
-		@output = ""
-		return @output if @values.empty?
-
-		#adapt for scale
-		# key line numbers
-		number_of_lines = (@scale * 2)  + 3 # each scale affects 2 verticals, add 3 for horizontals
-		last_line = number_of_lines - 1 #0 based
-		mind_line = @scale + 1 #height of one vertical and first horizontal line, 0 based
-
-		# prepare working container
-		display_values = []
-		number_of_lines.times { |line| display_values << "" } 
-
-		# generate the possible elements
-		horizontal_slices = [ " #{' ' * @scale} ", # " \s "
-			" #{@horizontal_character * @scale} "] # ie " - "
-		vertical_slices = [ " #{' ' *  @scale} ", # " \s "
-			"#{@vertical_character}#{' ' * @scale} ", " #{' ' * @scale}#{@vertical_character}",  #ie  "|\s ", " \s|",
-			"#{@vertical_character}#{' ' * @scale}#{@vertical_character}"] #ie  "|\s|"
-		special_slices = [ " ", "#{SPECIAL_CHARACTER}"] #ie "\s", "."
-
-		#actual conversion
-		#character by character, then line by line, appending
-		@values.each_char do |char|
-
-			char_on_grid = @character_encoding[char]
-			layer = 0 #top horizontal, top vertical, mid horizontal, bottom vertical, bottom horizontal
-
-			number_of_lines.times do |line|
-				display_values[line] += " " unless display_values[line].empty? #add spacing
-
-				#convert the encoding to its segment
-				if char_on_grid == :special
-					#special charaters, dont respect convention, ie ':'
-					if line == (mind_line - 1) || line == (mind_line + 1)
-						#only output on either side of mid line
-						display_values[line] +=  special_slices[1]
-					else
-						display_values[line] +=  special_slices[0]
-					end
-				#normal characters
-				elsif layer.even?
-					#horizontal, strings already scaled
-					display_values[line] += horizontal_slices[ char_on_grid[layer]]
-				else	
-					#vertical, parent loop copes with scaling
-					display_values[line] += vertical_slices[ char_on_grid[layer]]
-				end
-
-				#switch to next layer if needed, ie do not increment if need more vericals
-				if line == 0 ||  line == mind_line
-					#just done a horizontal layer next must be vertical
-					layer += 1
-				elsif line == (last_line - 1) ||line == (mind_line - 1)
-					#next layer is horizontal
-					layer += 1
-				end
-
-			end
-		end
-
-		#convert into single string
-		@output = display_values.join( "\n")
-		@output += "\n" unless @output.empty? #add trailing new line
-
-		@logger << { values: @values, scale: @scale, finished_at: Time.now, output: @output }
-		@output
+		@output = self.create_output( @values, @scale, @logger, 
+			@character_encoding, @horizontal_character, @vertical_character)
 	end
 
 end
